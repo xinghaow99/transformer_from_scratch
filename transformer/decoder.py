a7dc70b7491de9f496098d6b414c5e22323dcf0b
xinghaow <xinghaow99@gmail.com>
2022-09-05 19:27:52 +0800

    encoder & decoder

---
+++ transformer/decoder.py
@@ -0,0 +1,46 @@
+import imp
+import numpy as np
+from modules.embedding import Embedding
+from modules.dropout import Dropout
+from modules.linear import Linear
+from modules.softmax import Softmax
+from transformer.positional_encoding import PositionalEncoding
+from transformer.decoder_block import DecoderBlock
+class Decoder():
+    def __init__(self, optimizer, vocab_size, max_len, d_model, d_ff, num_attention_heads, block_num, dropout_rate, mask1, mask2, data_type):
+        self.d_model = d_model
+        self.embedding = Embedding(vocab_size, d_model, optimizer, data_type)
+        self.dropout = Dropout(dropout_rate, data_type)
+        self.positional_enconding = PositionalEncoding(max_len, d_model, data_type)
+        self.decoder_layers = [DecoderBlock(optimizer, d_model, d_ff, num_attention_heads, dropout_rate, mask1, mask2, data_type) for _ in range(block_num)]
+        self.fc = Linear(d_model, vocab_size, optimizer, True, data_type)
+        self.softmax = Softmax()
+
+    def forward(self, target, source, training):
+        target = self.embedding.forward(target) * np.sqrt(self.d_model)
+        target = self.positional_enconding.forward(target)
+        target = self.dropout.forward(target, training)
+        for decoder_layer in self.decoder_layers:
+            target =  decoder_layer.forward(target, source, training)
+        target = self.fc.forward(target)
+        target = self.softmax.forward(target)
+        return target
+
+    def backward(self, grad):
+        grad = self.softmax.backward(grad)
+        grad = self.fc.backward(grad)
+        grad_source_sum = 0
+        for decoder_layer in reversed(self.decoder_layers):
+            grad, grad_source = decoder_layer.backward(grad)
+            grad_source_sum += grad_source
+        grad = self.dropout.backward(grad)
+        grad = self.positional_enconding.backward(grad) * np.sqrt(self.d_model)
+        grad = self.embedding.backward(grad)
+        return grad, grad_source_sum
+
+    def update_weights(self):
+        self.fc.update_weights()
+        for decoder_layer in reversed(self.decoder_layers):
+            decoder_layer.update_weights()
+        self.embedding.update_weights()
+
--- transformer/decoder_block.py
+++ transformer/decoder_block.py
@@ -4,7 +4,7 @@ from modules.layer_normalization import LayerNormalization
 from transformer.multi_head_attention import MultiHeadAttention
 from transformer.position_wise_feed_forward import PositionWiseFeedForward
 class DecoderBlock():
-    def __init__(self, optimizer, d_model=512, d_ff=2048, num_attention_heads=8, dropout_rate=0.1, mask1=None, mask2=None, data_type=np.float32, training=True):
+    def __init__(self, optimizer, d_model=512, d_ff=2048, num_attention_heads=8, dropout_rate=0.1, mask1=None, mask2=None, data_type=np.float32):
         self.optimizer = optimizer
         self.d_model = d_model
         self.d_ff = d_ff
@@ -13,7 +13,6 @@ class DecoderBlock():
         self.mask1 = mask1
         self.mask2 = mask2
         self.data_type = data_type
-        self.training = training
         self.dropout = Dropout(self.dropout_rate)
         self.layernorm1 = LayerNormalization(self.optimizer, self.d_model, 1e-5, self.data_type)
         self.layernorm2 = LayerNormalization(self.optimizer, self.d_model, 1e-5, self.data_type)
@@ -22,17 +21,17 @@ class DecoderBlock():
         self.multi_head_att2 = MultiHeadAttention(self.optimizer, self.d_model, self.num_attention_heads, self.dropout_rate, self.mask2, self.data_type)
         self.ffn = PositionWiseFeedForward(self.optimizer, self.d_model, self.d_ff, self.dropout_rate, self.data_type)
 
-    def forward(self, target, source):
+    def forward(self, target, source, training=True):
         q, k, v = target, target, target
-        attention_output1 = self.multi_head_att1.forward(q, k, v, self.training)
-        attention_output1 = self.dropout.forward(attention_output1, self.training)
+        attention_output1 = self.multi_head_att1.forward(q, k, v, training)
+        attention_output1 = self.dropout.forward(attention_output1, training)
         target_ = self.layernorm1.forward(target + attention_output1)
         q, k, v = source, source, target_
-        attention_output2 = self.multi_head_att2.forward(q, k, v, self.training)
-        attention_output2 = self.dropout.forward(attention_output2, self.training)
+        attention_output2 = self.multi_head_att2.forward(q, k, v, training)
+        attention_output2 = self.dropout.forward(attention_output2, training)
         x = self.layernorm2.forward(target_ + attention_output2)
-        x_ = self.ffn.forward(x, self.training)
-        x_ = self.dropout.forward(x_, self.training)
+        x_ = self.ffn.forward(x, training)
+        x_ = self.dropout.forward(x_, training)
         x = self.layernorm3.forward(x + x_)
         return x
 
