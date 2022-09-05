a8e1901598d6f5a78ff61d048142989bad62864a
xinghaow <xinghaow99@gmail.com>
2022-09-05 13:47:07 +0800

    update gitignore; decoder block

--- transformer/decoder_block.py
+++ transformer/decoder_block.py
@@ -4,7 +4,7 @@ from modules.layer_normalization import LayerNormalization
 from transformer.multi_head_attention import MultiHeadAttention
 from transformer.position_wise_feed_forward import PositionWiseFeedForward
 class DecoderBlock():
-    def __init__(self, optimizer, d_model=512, d_ff=2048, num_attention_heads=8, dropout_rate=0.1, mask1=None, mask2=None, data_type=np.float32):
+    def __init__(self, optimizer, d_model=512, d_ff=2048, num_attention_heads=8, dropout_rate=0.1, mask1=None, mask2=None, data_type=np.float32, training=True):
         self.optimizer = optimizer
         self.d_model = d_model
         self.d_ff = d_ff
@@ -13,6 +13,7 @@ class DecoderBlock():
         self.mask1 = mask1
         self.mask2 = mask2
         self.data_type = data_type
+        self.training = training
         self.dropout = Dropout(self.dropout_rate)
         self.layernorm1 = LayerNormalization(self.optimizer, self.d_model, 1e-5, self.data_type)
         self.layernorm2 = LayerNormalization(self.optimizer, self.d_model, 1e-5, self.data_type)
@@ -20,3 +21,40 @@ class DecoderBlock():
         self.multi_head_att1 = MultiHeadAttention(self.optimizer, self.d_model, self.num_attention_heads, self.dropout_rate, self.mask1, self.data_type)
         self.multi_head_att2 = MultiHeadAttention(self.optimizer, self.d_model, self.num_attention_heads, self.dropout_rate, self.mask2, self.data_type)
         self.ffn = PositionWiseFeedForward(self.optimizer, self.d_model, self.d_ff, self.dropout_rate, self.data_type)
+
+    def forward(self, target, source):
+        q, k, v = target, target, target
+        attention_output1 = self.multi_head_att1.forward(q, k, v, self.training)
+        attention_output1 = self.dropout.forward(attention_output1, self.training)
+        target_ = self.layernorm1.forward(target + attention_output1)
+        q, k, v = source, source, target_
+        attention_output2 = self.multi_head_att2.forward(q, k, v, self.training)
+        attention_output2 = self.dropout.forward(attention_output2, self.training)
+        x = self.layernorm2.forward(target_ + attention_output2)
+        x_ = self.ffn.forward(x, self.training)
+        x_ = self.dropout.forward(x_, self.training)
+        x = self.layernorm3.forward(x + x_)
+        return x
+
+    def backward(self, grad):
+        grad_res1 = self.layernorm3.backward(grad)
+        grad = self.dropout.backward(grad_res1)
+        grad = self.ffn.backward(grad)
+        grad = grad_res1 + grad
+        grad_res2 = self.layernorm2.backward(grad)
+        grad = self.dropout.backward(grad_res2)
+        grad, grad_src_k , grad_src_v = self.multi_head_att2.backward(grad)
+        grad = grad_res2 + grad
+        grad_res3 = self.layernorm1.backward(grad)
+        grad = self.dropout.backward(grad_res3)
+        grad_tgt_q, grad_tgt_k, grad_tgt_v = self.multi_head_att1.backward(grad)
+        return grad_res3 + grad_tgt_q + grad_tgt_k + grad_tgt_v, grad_src_k + grad_src_v
+
+    def update_weights(self):
+        self.layernorm3.update_weights()
+        self.ffn.update_weights()
+        self.layernorm2.update_weights()
+        self.multi_head_att2.update_weights()
+        self.layernorm1.update_weights()
+        self.multi_head_att1.update_weights()
+
